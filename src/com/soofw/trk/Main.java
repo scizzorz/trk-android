110ab8f168cfb3975cbd70954073f816095c7b14
John Weachock <jweachock@gmail.com>
2013-08-01 13:26:55 -0400

    Animate tasks when deleting

--- src/com/soofw/trk/Main.java
+++ src/com/soofw/trk/Main.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.Transformation;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -42,25 +45,18 @@ public class Main extends Activity {
 				boolean checked = ((ListView)parent).isItemChecked(position);
 				if(checked) {
 					((ListView)parent).setItemChecked(position, false);
-					list.remove(position);
-					list.filter(omnibar.getText().toString());
-					adapter.notifyDataSetChanged();
-					list.write();
+					deleteItem(view, position);
 				}
 			}
 		});
 
 		omnibar.addTextChangedListener(new TextWatcher() {
 			@Override
-			public void afterTextChanged(Editable s) {
-			}
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			}
-			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				filterItems(s.toString());
 			}
+			@Override public void afterTextChanged(Editable s) {}
+			@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 		});
 	}
 
@@ -77,5 +73,50 @@ public class Main extends Activity {
 			omnibar.setText("");
 			list.write();
 		}
+	}
+
+	// Many thanks to https://github.com/paraches/ListViewCellDeleteAnimation for this code
+	public void deleteItem(final View view, final int index) {
+		AnimationListener al = new AnimationListener() {
+			@Override
+			public void onAnimationEnd(Animation arg) {
+				list.remove(index);
+				list.filter(omnibar.getText().toString());
+				adapter.notifyDataSetChanged();
+				list.write();
+			}
+			@Override public void onAnimationRepeat(Animation anim) {}
+			@Override public void onAnimationStart(Animation anim) {}
+		};
+
+		collapseView(view, al);
+	}
+
+	public void collapseView(final View view, final AnimationListener al) {
+		final int initialHeight = view.getMeasuredHeight();
+
+		Animation anim = new Animation() {
+			@Override
+			protected void applyTransformation(float interpolatedTime, Transformation t) {
+				if(interpolatedTime == 1) {
+					view.getLayoutParams().height = initialHeight;
+					view.requestLayout();
+				} else {
+					view.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+					view.requestLayout();
+				}
+			}
+
+			@Override
+			public boolean willChangeBounds() {
+				return true;
+			}
+		};
+
+		if(al != null) {
+			anim.setAnimationListener(al);
+		}
+		anim.setDuration(200);
+		view.startAnimation(anim);
 	}
 }
